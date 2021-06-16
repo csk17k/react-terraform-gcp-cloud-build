8b2d4f189ee9b1d78eb23b9dad44b6d6bff86802
csk17k <84774760+csk17k@users.noreply.github.com>
2021-06-16 19:51:38 +0530

    Update 02-variables.tf

--- deploy/02-variables.tf
+++ deploy/02-variables.tf
@@ -1,15 +1,15 @@
 variable "project" {
-  default = "gcp-terraform-299512"
+  default = "gcp-service-line-314413"
 }
 
 variable "credentials_filepath" {
-  default = "/Users/matthewlau/gcp-creds/gcp-creds.json"
+  default = "credentials.json"
 }
 
 variable "bucket_name" {
-  default = "www.react.matlau.co.uk"
+  default = "react-gcp"
 }
 
 variable "cloud_source_repo_name" {
-  default = "github_matthewcylau_react-terraform-gcp-cloud-build"
+  default = "github_csk17k_react-terraform-gcp-cloud-build"
 }
